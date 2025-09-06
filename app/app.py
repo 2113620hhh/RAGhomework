67382a0f50c6266df1addf164ae89b38d92df184
“2113620hhh” <2025768663@qq.com>
2025-09-06 15:52:23 +0800

    一次正常的提交

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify, Response
 import json
 import os
+import sys
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if root_dir not in sys.path:
+    sys.path.append(root_dir)
 import re
 import logging
 from coord_agent import *
